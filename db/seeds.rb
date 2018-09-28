763cd63c325d1865ebe10cfb4485472396e4b8d3
Matthew Sampson <mbsampson89@gmail.com>
2018-09-28 09:32:01 -0700

    Add projects/experience seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Experience.create(
+  title: 'Company Name',
+  position: 'Software Developer',
+  content: 'Nunc non mi arcu. Integer dui ligula, tempor id suscipit ut, ornare pretium purus. Nulla facilisi. Etiam a cursus leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fringilla interdum aliquet. Aenean facilisis mattis massa, quis bibendum turpis consequat nec. Cras est magna, placerat vitae enim non, aliquet consectetur est. Nullam faucibus turpis in turpis pulvinar, tempus dignissim leo tristique. Vivamus aliquet dui sed cursus dignissim. Ut fermentum neque eget velit fermentum iaculis. Curabitur vel augue in mauris venenatis suscipit in eu arcu.',
+  start_date: 'July 2015',
+  end_date: 'September 2018'
+)
+
+Experience.create(
+  title: 'Company Name',
+  position: 'Software Developer',
+  content: 'Nunc non mi arcu. Integer dui ligula, tempor id suscipit ut, ornare pretium purus. Nulla facilisi. Etiam a cursus leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fringilla interdum aliquet. Aenean facilisis mattis massa, quis bibendum turpis consequat nec. Cras est magna, placerat vitae enim non, aliquet consectetur est. Nullam faucibus turpis in turpis pulvinar, tempus dignissim leo tristique. Vivamus aliquet dui sed cursus dignissim. Ut fermentum neque eget velit fermentum iaculis. Curabitur vel augue in mauris venenatis suscipit in eu arcu.',
+  start_date: 'July 2015',
+  end_date: 'September 2018'
+)
+
+Experience.create(
+  title: 'Company Name',
+  position: 'Software Developer',
+  content: 'Nunc non mi arcu. Integer dui ligula, tempor id suscipit ut, ornare pretium purus. Nulla facilisi. Etiam a cursus leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fringilla interdum aliquet. Aenean facilisis mattis massa, quis bibendum turpis consequat nec. Cras est magna, placerat vitae enim non, aliquet consectetur est. Nullam faucibus turpis in turpis pulvinar, tempus dignissim leo tristique. Vivamus aliquet dui sed cursus dignissim. Ut fermentum neque eget velit fermentum iaculis. Curabitur vel augue in mauris venenatis suscipit in eu arcu.',
+  start_date: 'July 2015',
+  end_date: 'September 2018'
+)
+
+Project.create(
+  title: 'MovieGrumps',
+  bg_image: 'movie-seats-bg.png',
+  technologies: ['ruby', 'postgresql', 'html', 'rails']
+)
+
+Project.create(
+ title: 'R3Adspace',
+ bg_image: 'adlisting.png',
+ technologies: ['ruby', 'rails', 'postgresql', 'html', 'jquery']
+)
